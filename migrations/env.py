5d95def49fd9cce0724f2febbd1056f741adbc86
Josh Chou <626661+josh-chou@users.noreply.github.com>
2020-03-27 19:34:22 -0700

    add create_engine

--- migrations/env.py
+++ migrations/env.py
@@ -2,7 +2,7 @@
 import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 
 from alembic import context
